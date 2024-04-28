db953a3d8b1ee18dc359c2aca57342b9984ed59c
Giorbis Miguel <giorbismiguel@gmail.com>
2024-04-27 22:13:12 -0600

    Fix error with

--- apps/backoffice/src/modules/authentication/pages/SignUp.tsx
+++ apps/backoffice/src/modules/authentication/pages/SignUp.tsx
@@ -3,15 +3,15 @@ import { H1 } from "mui-react-common";
 import SignUpContainer from "modules/authentication/container/SignUpContainer";
 import { useTranslation } from "react-i18next";
 
-const SignUp = () => {
+function SignUp() {
   const { t } = useTranslation("authentication");
 
   return (
     <>
-      <H1 textAlign={"center"}>{t("loginTitle")}</H1>
+      <H1 textAlign="center">{t("loginTitle")}</H1>
       <SignUpContainer />
     </>
   );
-};
+}
 
 export default memo(SignUp);
